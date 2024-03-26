e9854090523ce44d307df11f8b5be1241058b452
riasetiaji <132030153+riasetiaji@users.noreply.github.com>
2024-03-26 11:49:49 +0700

    Membuat controller dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        // Pengguna yang sedang login
+        $user = Auth::user();
+
+        // Memastikan pengguna ada dan memiliki role_id
+        if ($user && $user->role_id) {
+            // Mengambil data jenis_id dari tabel master_role_id yang sesuai dengan role_id pengguna
+            $jenisId = DB::table('users')
+                        ->join('master_role_id', 'users.role_id', '=', 'master_role_id.id')
+                        ->where('users.id', $user->id)
+                        ->value('master_role_id.jenis_id');
+
+            // Meneruskan data pengguna dan jenis_id ke tampilan dashboard
+            return view('dashboard', ['user' => $user, 'jenis_id' => $jenisId]);
+        } else {
+            // Redirect ke halaman login jika pengguna tidak terautentikasi atau tidak memiliki role_id
+            return redirect()->route('login');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Login berdasarkan role user
-Route::get('/dashboard', function () {
-    // Pengguna yang sedang login
-    $user = Auth::user();
-    // Memastikan pengguna ada dan memiliki role_id
-    if ($user && $user->role_id) {
-        // Mengambil data jenis_id dari tabel master_role_id yang sesuai dengan role_id pengguna
-        $jenisId = DB::table('users')
-                    ->join('master_role_id', 'users.role_id', '=', 'master_role_id.id')
-                    ->where('users.id', $user->id)
-                    ->value('master_role_id.jenis_id');
-        // Meneruskan data pengguna dan jenis_id ke tampilan dashboard
-        return view('dashboard', ['user' => $user, 'jenis_id' => $jenisId]);
-    } else {
-        // Redirect ke halaman login jika pengguna tidak terautentikasi atau tidak memiliki role_id
-        return redirect()->route('login');
-    }
-    // Meneruskan data pengguna ke tampilan dashboard
-    //return view('dashboard', ['user' => $user]);
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Login dashboard berdasarkan role user
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
